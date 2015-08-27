01a86172087f2e127dca5f6d6bda55e5a1637894
Artem Alexeew <artem@aft.ru>
2015-08-27 10:23:50 +0300

    Refs #NOTICKET

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
 	session_start();
 	$_SESSION['token'] = md5(uniqid(rand(), true));
 ?>
+<b>Example:</b>
+<div>
+5 5<br/>
+1 2 N<br/>
+LMLMLMLMM<br/>
+3 3 E<br/>
+MMRMMRMRRM<br/>
+</div>
 <form action="post.php" method="POST">
 	<input type="hidden" name="token" value="<?=$_SESSION['token'];?>"/>
 	<textarea name="input" rows="5"></textarea>
